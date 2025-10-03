eef934e927ed6e8c0c39895766dfa3a3f011cc76
psjw <dlrudtn108@gmail.com>
2025-10-03 20:16:07 +0900

    3. 프로젝트 세팅

---
+++ monolithic/src/test/java/com/wardiz/monolithic/MonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wardiz.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
